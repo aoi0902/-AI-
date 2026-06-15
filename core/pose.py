e9b981f57b6ca4e8d076f8d353fa9de78aa0aefa
taiseitaiseita <taisei672734@icloud.com>
2026-06-15 14:45:14 +0900

    pose.py:骨格推定クラスを作成

---
+++ core/pose.py
@@ -0,0 +1,32 @@
+import cv2
+import mediapipe as mp
+
+BaseOptions = mp.tasks.BaseOptions
+PoseLandmarker = mp.tasks.vision.PoseLandmarker
+PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+class PoseEstimator:
+    def __init__(self, model_path='models/pose_landmarker.task'):
+        options = PoseLandmarkerOptions(
+            base_options=BaseOptions(model_asset_path=model_path),
+            running_mode=VisionRunningMode.IMAGE
+        )
+        self.landmarker = PoseLandmarker.create_from_options(options)
+
+    def estimate(self, frame_bgr):
+        rgb = cv2.cvtColor(frame_bgr, cv2.COLOR_BGR2RGB)
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb)
+        result = self.landmarker.detect(mp_image)
+        if result.pose_landmarks:
+            return result.pose_landmarks[0]
+        return None
+
+    def close(self):
+        self.landmarker.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        self.close()
